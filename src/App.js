f5bfc3199a1c18380a6b929fd8f054ded88b12c6
coder054 <binhdinhgiaftu@gmail.com>
2019-01-24 11:16:01 +0700

    header

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import Notfound from "./components/Notfound"
 import ForecastOneCity from "./components/ForecastOneCity"
 import Home from "./components/Home"
+import Header from './components/Header';
 import "bootstrap/dist/css/bootstrap.css"
+
+
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className="App container">
+					<Header></Header>
 					<Switch>
 						<Route path="/forecast/:cityname" component={ForecastOneCity} />
 						<Route path="/" exact component={Home} />
---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import { withRouter } from "react-router-dom"
+class Header extends Component {
+	state = {
+		cityname: ''
+	}
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.props.history.push(this.state.cityname)
+	}
+	render() {
+		return (
+			<nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
+				<Link className="navbar-brand" to="/">
+					{" "}
+					Weather{" "}
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="mr-auto" />
+					<form
+						onSubmit={this.handleSubmit}
+						className="form-inline my-2 my-lg-0"
+					>
+						<input
+							value={this.state.cityname}
+							onChange={(e)=> this.setState({cityname: e.target.value})  }
+							className="form-control mr-sm-2"
+							type="search"
+							placeholder="Search city"
+							aria-label="Search"
+						/>
+						<button
+							className="btn btn-outline-success my-2 my-sm-0"
+							type="submit"
+						>
+							Search
+						</button>
+					</form>
+				</div>
+			</nav>
+		)
+	}
+}
+
+export default withRouter(Header)
